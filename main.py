c392c6fcb70c8806a2e2e6cd89b020e59f991184
osiakmikolaj <osiakmikolaj@gmail.com>
2025-04-09 21:47:17 +0200

    .gitignore, venv and main file

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+# following ensures that the main() function is only called when 
+# this file is run directly; it won't run if it's imported as a module. 
+if __name__ == "__main__":
+    main()
