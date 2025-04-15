c909aea38efd54deae7a688866939b4456b3e91f
osiakmikolaj <osiakmikolaj@gmail.com>
2025-04-15 22:49:43 +0200

    a game loop was created

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
 
 # following ensures that the main() function is only called when 
 # this file is run directly; it won't run if it's imported as a module. 
