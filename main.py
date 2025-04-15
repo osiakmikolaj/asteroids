b1470fe63b61b06ca91e77e8f57cd36f82ffda05
osiakmikolaj <osiakmikolaj@gmail.com>
2025-04-15 23:02:42 +0200

    delta and 60 fps limit

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+            
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 # following ensures that the main() function is only called when 
 # this file is run directly; it won't run if it's imported as a module. 
